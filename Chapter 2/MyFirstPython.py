4492b3d535241a63a90aedfd1090aff45c9b9b6b
Ethan Smith <ethanjames1725@gmail.com>
2026-05-27 12:23:17 +0200

    created new file: Chapter 2/MyFirstPython.py created new file: Chapter 2/NumberGuessingGame.py

---
+++ Chapter 2/MyFirstPython.py
@@ -0,0 +1,6 @@
+x = input("Enter a number: ")
+x = int(x)
+if x > 5:
+    print("x is greater than 5")
+else:
+    print("x is less than or equal to 5")
---
+++ Chapter 2/NumberGuessingGame.py
@@ -0,0 +1,19 @@
+import random
+
+secret_number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while True:
+    guess = int(input("Your guess: "))
+    attempts += 1
+
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print(f"You got it in {attempts} guesses!")
+        break
