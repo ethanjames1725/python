18d8a50f151258312bbbc56067a4934cc64b6653
Ethan Smith <ethanjames1725@gmail.com>
2026-06-02 13:25:35 +0200

    completed chapter 3 exercises.

--- Classwork/chapter_3/cars.py
+++ Classwork/chapter_3/cars.py
@@ -1,3 +1,25 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort()
 print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("\n\nHere is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("\n\nHere is the original list:")
+print(cars)
+cars.reverse()
+print("\nHere is the reversed list:")
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(f"\nI have {len(cars)} cars in my list.")
