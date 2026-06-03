a9af52bf07f3b45dddcb4bed6a6c3af8e6e886f1
Ethan Smith <ethanjames1725@gmail.com>
2026-06-03 12:09:00 +0200

    created new files and made changes to existing files.

--- Classwork/chapter_2/numbers.py
+++ Classwork/chapter_2/numbers.py
@@ -6,3 +6,12 @@ print(f"3 ** 2 = {3 ** 2}")
 print(f"10 ** 6 = {10 ** 6}")
 print(f"2 + 3 * 4 = {2 + 3 * 4}")
 print(f"(2 + 3) * 4 = {(2 + 3) * 4}")
+print(f"0.1 + 0.2 = {0.1 + 0.2}")
+print(f"0.1 + 0.2 == 0.3: {0.1 + 0.2 == 0.3}")
+print(f"0.11 + 0.22 == 0.33: {0.11 + 0.22 == 0.33}")
+universe_age = 14_000_000_000
+print(universe_age)
+x,y,z = 1,2,3
+print(f"{x}{y}{z}")
+MAX_CONNECTIONS = 5000
+print(f"Max Connections: {MAX_CONNECTIONS}")
