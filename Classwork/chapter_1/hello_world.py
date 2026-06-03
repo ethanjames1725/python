341d167b3315b374dfd58ce9c3887011c0c7e0ef
Ethan Smith <ethanjames1725@gmail.com>
2026-06-03 12:57:21 +0200

    added code summary to hello_world.py

--- Classwork/chapter_1/hello_world.py
+++ Classwork/chapter_1/hello_world.py
@@ -1,1 +1,3 @@
+#Summary: print("Hello Python World!") calls Python's built-in print function, which outputs the string Hello Python World! 
+#to the console, followed by a newline.
 print("Hello Python World!")
