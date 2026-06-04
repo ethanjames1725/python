2bad1ea1d125a448fb552889198eaf248c242748
Ethan Smith <ethanjames1725@gmail.com>
2026-06-04 15:16:12 +0200

    new file:   Classwork/chapter_3/intentional_error.py

---
+++ Classwork/chapter_3/intentional_error.py
@@ -0,0 +1,6 @@
+# This code is intentionally incorrect to demonstrate error handling in Python.
+
+# list_of_numbers = [1, 2, 3, 4, 5]
+# print(list_of_numbers[10])  # This will raise an IndexError since index 10 does not exist in the list.
+# remove_item = list_of_numbers.remove(6)  # This will raise a ValueError since 6 is not in the list.
+# print(remove_item)  # This will print None since the remove() method does not return anything.
