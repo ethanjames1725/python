d248bfad1e35e7da01cf91c54c7b744f1e8a0c71
Ethan Smith <ethanjames1725@gmail.com>
2026-06-05 12:43:35 +0200

    Updated Magicians.py

--- Classwork/chapter_4/magicians.py
+++ Classwork/chapter_4/magicians.py
@@ -1,3 +1,5 @@
+# This program demonstrates how to loop through a list of magicians and print their names, 
+# as well as a personalized message for each magician.
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
         print(magician)
