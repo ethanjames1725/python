c94a91c589e43a3237bdda4f1f114aa535c47da3
Ethan Smith <ethanjames1725@gmail.com>
2026-06-10 13:27:28 +0200

    continued with chapter 5

---
+++ Classwork/chapter_5/favourite_fruits.py
@@ -0,0 +1,11 @@
+favourite_fruits = ['apples', 'mangos', 'avocados']
+if 'mangos' in favourite_fruits:
+    print("You really like mangos!")
+if 'bananas' in favourite_fruits:
+    print("You really like bananas!")
+if 'pears' in favourite_fruits:
+    print("You really like pears!")
+if 'apples' in favourite_fruits:
+    print("You really like apples!")
+if 'avocados' in favourite_fruits:
+    print("You really like avocados!")
