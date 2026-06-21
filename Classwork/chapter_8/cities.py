e6ada1914bf9973ec8e30f9884a5f4e24bf6dd3c
Ethan Smith <ethanjames1725@gmail.com>
2026-06-21 12:42:46 +0200

    continue with chapter 8

---
+++ Classwork/chapter_8/cities.py
@@ -0,0 +1,8 @@
+"""Summary"""
+def describe_city(city_name, city_country='south africa'):
+    """Display info about a city."""
+    print(f"\n{city_name.title()} is in {city_country.title()}.")
+
+describe_city('cape town')
+describe_city('johannesburg')
+describe_city('london', city_country='england')
---
+++ Classwork/chapter_8/formatted_name.py
@@ -0,0 +1,32 @@
+"""summary"""
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+
+# Full name with 3 arguments:
+def get_formatted_name_1(first_name, middle_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {middle_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name_1('john', 'lee', 'hooker')
+print(musician)
+
+# Full name with an optional middle name argument
+def get_formatted_name_2(first_name, last_name, middle_name=''):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+musician = get_formatted_name_2('jimi', 'hendrix')
+print(musician)
+
+musician = get_formatted_name_2('john', 'hooker', 'lee')
+print(musician)
